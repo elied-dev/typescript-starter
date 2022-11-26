5cddba0d648f73784fedd3a833ddfcf999c1d5b6
Elie Drai <elie.drai.work@gmail.com>
2022-11-26 22:32:21 +0200

    fix: removing log quitting app

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,4 @@ import { AppLogger } from './logger/pino.logger';
 // Run all the program inside this async function
 (async () => {
   AppLogger.debug('Starting app...');
-
-  AppLogger.debug('Quitting app...');
 })();
