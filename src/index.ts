e59a3ff7ccfece00d34eebe6aa9df434b95dd557
Elie Drai <elie.drai.work@gmail.com>
2022-11-26 22:29:50 +0200

    feat: add end log

--- src/index.ts
+++ src/index.ts
@@ -3,4 +3,6 @@ import { AppLogger } from './logger/pino.logger';
 // Run all the program inside this async function
 (async () => {
   AppLogger.debug('Starting app...');
+
+  AppLogger.debug('Quitting app...');
 })();
